648cebddcaf2bd6cd531a2498f0f41e585f017c1
MauryRosa <rosalesmauricio847@gmail.com>
2024-08-21 22:58:25 -0600

    Add files via upload

---
+++ Ejercicio 5/js/main.js
@@ -0,0 +1,13 @@
+function convertirGradosARadianes(grados) {
+    return grados * (Math.PI / 180);
+}
+
+let grados = parseFloat(prompt("Ingrese el valor en grados:"));
+
+if (isNaN(grados)) {
+    console.log("Por favor, ingrese un número válido.");
+} else {
+    let radianes = convertirGradosARadianes(grados);
+    console.log(`${grados} grados son equivalentes a ${radianes} radianes`);
+}
+
---
+++ Ejercicio 6/js/main.js
@@ -0,0 +1,15 @@
+function esBisiesto(año) {
+    if ((año % 4 === 0 && año % 100 !== 0) || (año % 400 === 0)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+let año = prompt("Ingresa un año:");
+
+if (esBisiesto(año)) {
+    console.log(`${año} es un año bisiesto.`);
+} else {
+    console.log(`${año} no es un año bisiesto.`);
+}
